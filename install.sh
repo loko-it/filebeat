ff656017a08b506534e65ade3d5eec44c10549ad
loko-it <45261956+loko-it@users.noreply.github.com>
2020-02-21 11:46:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ echo " done."
 
 #Install new version
 echo -n "Installing filebeat..."
-/usr/sbin/pkg add -f https://github.com/loko-it/filebeat/raw/master/beats-6.7.1.txz
+/usr/sbin/pkg add -f https://github.com/loko-it/filebeat/raw/master/beats-6.8.6.txz
 echo " done."
 
 # Make filebeat auto start at boot
